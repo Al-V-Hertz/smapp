599c7ec10c794d3b7ad592ef4be813bcb7fdb188
Al-V-Hertz <albert.alay-ay@acacia-soft.com>
2020-03-17 11:18:59 +0800

    added design in landing html

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -2,17 +2,17 @@
 <html lang="en">
 <head>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/landing.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sarimanok</title>
 </head>
 <body>
-    <div class="background"></div> {{--full image background--}}
-    <div class="cover"> {{--color with opacity--}}
-        <div class="promo"> {{--text--}}
+    <div class="cover">
+        <div class="promo">
             <h1>Sarimanok</h1>
-            <p>We Cook Better than your Grandmothers</p>
+            <p>We Cook Better than your Grandmothers.</p>
             <a href="/signin">Sign In</a><a href="signup">Sign Up</a>
         </div>
     </div>
