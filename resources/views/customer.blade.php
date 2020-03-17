0fda700e384b2e87674d8b43f65201b710a31fe8
Al-V-Hertz <albert.alay-ay@acacia-soft.com>
2020-03-17 12:23:19 +0800

    added menu files: css and html

--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -2,19 +2,14 @@
 <html lang="en">
 <head>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/landing.css">
+    @yield('links')
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sarimanok</title>
+    <title>Sarimanok - @yield('subtitle')</title>
 </head>
 <body>
-    <div class="cover">
-        <div class="promo">
-            <h1>Sarimanok</h1>
-            <p>We Cook Better than your Grandmothers.</p>
-            <a href="/menu">Go to Menu</a>
-        </div>
-    </div>
+    @yield('content')
 </body>
+@yield('scripts')
 </html>
---
+++ resources/views/menu.blade.php
@@ -0,0 +1,15 @@
+@extends('customer')
+@section('subtitle', 'Menu')
+@section('links')
+    <link rel="stylesheet" href="css/menu.css">
+@endsection
+@section('content')
+    <div class="container">
+        <div class="jumbotron jumbotron-fluid">
+            <h1>Menu</h1>
+        </div>
+        <div class="slide">
+            
+        </div>
+    </div>
+@endsection
