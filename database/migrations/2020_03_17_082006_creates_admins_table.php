500c775da48a4de44bd856d380fccea468cb354e
Al-V-Hertz <albert.alay-ay@acacia-soft.com>
2020-03-17 18:49:46 +0800

    added adminController; added model and migration

---
+++ database/migrations/2020_03_17_082006_creates_admins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatesAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('fname');
+            $table->string('mname');
+            $table->string('lname');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
